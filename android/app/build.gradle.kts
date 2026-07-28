9a6479d856c65704895baf194901a8049ef1ac98
Clement <everlefeu33@gmail.com>
2026-07-28 19:04:03 +0200

    Build appbundle?

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,14 +33,14 @@ android {
         versionName = flutter.versionName
     }
 
-    signingConfigs {
-        create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
-            storePassword = keystoreProperties["storePassword"] as String
-        }
-    }
+//    signingConfigs {
+//        create("release") {
+//            keyAlias = keystoreProperties["keyAlias"] as String
+//            keyPassword = keystoreProperties["keyPassword"] as String
+//            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+//            storePassword = keystoreProperties["storePassword"] as String
+//        }
+//    }
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
